ab7e16f4ef448fdaf324ac44aee06759cc3f01bd
Foundit3923 <30578691+Foundit3923@users.noreply.github.com>
2021-01-10 16:47:37 -0500

    Comments

--- SMART_CODE_1.h
+++ SMART_CODE_1.h
@@ -55,33 +55,44 @@ int search_test (unsigned char* query_array,
     string_check[0] = &query_array[0];
 
     int_check[0] = LAST_BITS_ON;
-
+    
+    //While there's text left
     while(!(&*t_w.c > &st[text_len-1])) {
-
+        
+        //Make a single character mask
         value = (~((*t_w.i) ^ query_matches * (*char_ptr)) & ~((*t_w.i) ^ LAST_BITS_ON * (*char_ptr)));
-
+        
+        //Recude the single character mask to a bool array 
         shift_four = value & (value >> 4) & 0xF0F0F0F0F0F0F0FUL;
         query_matches = ((shift_four & (shift_four >> 2)) &
                          ((shift_four & (shift_four >> 2)) >> 1)) * (bool) hassetbyte(value);
-
+        
+        //check IF query_matches is 0
         check = (bool) query_matches;
-
+        
+        //If we get to the last character in the query and there's a match
         if((&char_ptr[0] == &query_array[query_len-1]) && check){
             //result += __builtin_popcount(query_matches);
+            //Count
             result += ((uint8_t) (query_matches + query_matches/255));
+            //Reset everything
             text_offset -= mismatch_move_count;
             query_matches = 0;
             check = false;
             mismatch_move_count = 0;
         }
-
+        
+        //increase or reset char_ptr
         string_check[1] = char_ptr + 1;
         char_ptr = string_check[check];
-
+        
+        //Increment mismatch_move_count, increment text_offset, set t_w.c to the new location
         t_w.c = &st[(text_offset += ((8 - (mismatch_move_count += check)) * !(check)) + check)];
-
+        
+        //Reset mismatch_move_count if mismatch
         mismatch_move_count = mismatch_move_count * check;
-
+        
+        //Reset query_matches if mismatch
         int_check[1] = query_matches;
         query_matches = int_check[check];
     }
