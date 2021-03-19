df7c7874a65be0630e9e703b24081be9e519bc04
Foundit3923 <30578691+Foundit3923@users.noreply.github.com>
2021-03-19 08:04:18 -0400

    Need to run tests

--- concurrent_BM.h
+++ concurrent_BM.h
@@ -1,5 +1,5 @@
 //
-// Created by michael on 1/23/21.
+// Created by olson on 3/17/2021.
 //
 
 
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include <math.h>
+#include <time.h>
 
 
 #define LAST_BITS_ON 0x101010101010101UL
@@ -19,6 +20,7 @@
 #define HALF_BITS_ON_2 0x10001UL
 #define hassetbyte(v) ((~(v) - 0x0101010101010101UL) & (v) & 0x8080808080808080UL)
 #define reduce(v) (((~v - LAST_BITS_ON) ^ ~v) & SIGNIFICANT_BITS_ON) >> 7
+#define NO_OF_CHARS 256
 
 union Window {
     uint64_t* i;
@@ -48,26 +50,49 @@ union Matches {
 union Offset {
     uint64_t* i;
     int o[8];
-};
+}Offset;
+
+union BadChar {
+    uint64_t* i;
+    int o[9];
+}BadChar;
+
+int max (int a, int b) { return (a > b)? a: b; }
+
+void badCharheuristic( char *str, int size, int badchar[NO_OF_CHARS]) {
+    int i;
+
+    for (i = 0; i < NO_OF_CHARS; i++){
+        badchar[i] = -1;
+    }
+
+    for (i = 0; i < size; i++){
+        badchar[(int) str[i]] = i;
+    }
+}
 
 int search_test (unsigned char* query_array,
                  int query_len,
                  unsigned char* st,
                  int text_len) {
 
+    int badchar[NO_OF_CHARS];
+
+    badCharheuristic(query_array,query_len,badchar);
+
     int mismatch_move_count = 0;
     int result = 0;
     int last_query_location = query_len - 1;
     int text_section_size = floor(text_len / 8);
     union Offset text_offset;
-    text_offset.o[0] = text_section_size;
-    text_offset.o[1] = floor(text_section_size * 2);
-    text_offset.o[2] = floor(text_section_size * 3);
-    text_offset.o[3] = floor(text_section_size * 4);
-    text_offset.o[4] = floor(text_section_size * 5);
-    text_offset.o[5] = floor(text_section_size * 6);
-    text_offset.o[6] = floor(text_section_size * 7);
-    text_offset.o[7] = floor(text_section_size * 8);
+    text_offset.o[0] = text_section_size + (query_len-1);
+    text_offset.o[1] = floor(text_section_size * 2) + (query_len-1);
+    text_offset.o[2] = floor(text_section_size * 3) + (query_len-1);
+    text_offset.o[3] = floor(text_section_size * 4) + (query_len-1);
+    text_offset.o[4] = floor(text_section_size * 5) + (query_len-1);
+    text_offset.o[5] = floor(text_section_size * 6) + (query_len-1);
+    text_offset.o[6] = floor(text_section_size * 7) + (query_len-1);
+    text_offset.o[7] = floor(text_section_size * 8) + (query_len-1);
 
     union Offset query_offset;
     query_offset.o[0] = last_query_location;
@@ -91,12 +116,14 @@ int search_test (unsigned char* query_array,
     union Index idx;
 
     union Index last_idx;
-    *last_idx.i = LAST_BITS_ON * (query_len - 1);
+    last_idx.i = LAST_BITS_ON * (query_len - 1);
 
     union MultiChar m_c;
 
     union Matches query_matches;
     query_matches.i = LAST_BITS_ON;
+
+    union Matches inverse_matches;
 
     union Matches last_query_match;
     uint64_t value;
@@ -104,33 +131,17 @@ int search_test (unsigned char* query_array,
     uint64_t finished_queries;
     uint64_t finsihed_qeuries_offset;
     uint64_t finished_queries_offset_plus_query_len;
+    uint64_t found_queries;
 
     union Matches match_comparison;
 
+    union BadChar bc;
+
     char_ptr = &query_array[0];
 
-    while(text_offset.o[7] <= (text_len-query_len) && (*query_matches.i != 0UL)) {
-        *last_query_match.i = *query_matches.i;
+    while(text_offset.o[7] <= text_len) {// && *query_matches.i != 0UL) {
 
-        //increment text_offset
-        text_offset.o[0] += query_matches.c[0];
-        text_offset.o[1] += query_matches.c[1];
-        text_offset.o[2] += query_matches.c[2];
-        text_offset.o[3] += query_matches.c[3];
-        text_offset.o[4] += query_matches.c[4];
-        text_offset.o[5] += query_matches.c[5];
-        text_offset.o[6] += query_matches.c[6];
-        text_offset.o[7] += query_matches.c[7];
-
-        //increment query_offset
-        query_offset.o[0] -= query_matches.c[0];
-        query_offset.o[1] -= query_matches.c[1];
-        query_offset.o[2] -= query_matches.c[2];
-        query_offset.o[3] -= query_matches.c[3];
-        query_offset.o[4] -= query_matches.c[4];
-        query_offset.o[5] -= query_matches.c[5];
-        query_offset.o[6] -= query_matches.c[6];
-        query_offset.o[7] -= query_matches.c[7];
+        //*last_query_match.i = *query_matches.i;
 
         //build multimask
 
@@ -156,6 +167,93 @@ int search_test (unsigned char* query_array,
         //check multi-mask against text
         value = ~((*t_w.i) ^ *query_matches.i);
 
+        //if matches
+        if((bool)(*query_matches.i = reduce(value))) {
+            //decrease matches by 1
+            *text_offset.i -= *query_matches.i;
+            /*
+            text_offset.o[0] -= query_matches.c[0];
+            text_offset.o[1] -= query_matches.c[1];
+            text_offset.o[2] -= query_matches.c[2];
+            text_offset.o[3] -= query_matches.c[3];
+            text_offset.o[4] -= query_matches.c[4];
+            text_offset.o[5] -= query_matches.c[5];
+            text_offset.o[6] -= query_matches.c[6];
+            text_offset.o[7] -= query_matches.c[7];
+             */
+
+            //increment query_offset
+            *query_offset.i += *bc.i;
+            /*
+            query_offset.o[0] -= query_matches.c[0];
+            query_offset.o[1] -= query_matches.c[1];
+            query_offset.o[2] -= query_matches.c[2];
+            query_offset.o[3] -= query_matches.c[3];
+            query_offset.o[4] -= query_matches.c[4];
+            query_offset.o[5] -= query_matches.c[5];
+            query_offset.o[6] -= query_matches.c[6];
+            query_offset.o[7] -= query_matches.c[7];
+             */
+
+        }
+        //if mismatches
+        if((*query_matches.i & LAST_BITS_ON) == LAST_BITS_ON) {
+            //shift by badchar value
+                //to get mismatches
+                    //invert query_matches then AND with LAST_BITS_ON
+            //only mismatches will be shifted
+            *inverse_matches.i = (~*query_matches.i & LAST_BITS_ON);
+
+            bc.o[0] = badchar[text_offset.o[0]] * inverse_matches.c[0];
+            bc.o[1] = badchar[text_offset.o[1]] * inverse_matches.c[1];
+            bc.o[2] = badchar[text_offset.o[2]] * inverse_matches.c[2];
+            bc.o[3] = badchar[text_offset.o[3]] * inverse_matches.c[3];
+            bc.o[4] = badchar[text_offset.o[4]] * inverse_matches.c[4];
+            bc.o[5] = badchar[text_offset.o[5]] * inverse_matches.c[5];
+            bc.o[6] = badchar[text_offset.o[6]] * inverse_matches.c[6];
+            bc.o[7] = badchar[text_offset.o[7]] * inverse_matches.c[7];
+
+            //increment text_offset by badchar value
+            *text_offset.i += *bc.i;
+            /*
+            text_offset.o[0] += bc.o[0];
+            text_offset.o[1] += bc.o[1];
+            text_offset.o[2] += bc.o[2];
+            text_offset.o[3] += bc.o[3];
+            text_offset.o[4] += bc.o[4];
+            text_offset.o[5] += bc.o[5];
+            text_offset.o[6] += bc.o[6];
+            text_offset.o[7] += bc.o[7];
+             */
+
+            //increment text_offset by badchar value
+            *query_offset.i += *bc.i;
+            /*
+            query_offset.o[0] += bc.o[0];
+            query_offset.o[1] += bc.o[1];
+            query_offset.o[2] += bc.o[2];
+            query_offset.o[3] += bc.o[3];
+            query_offset.o[4] += bc.o[4];
+            query_offset.o[5] += bc.o[5];
+            query_offset.o[6] += bc.o[6];
+            query_offset.o[7] += bc.o[7];
+             */
+        }
+        //if index < 0 for any matches
+        if((bool)(found_queries = reduce(~((*query_offset.i) ^ (-1 * LAST_BITS_ON))))) {
+            //shift by m+1
+            result += ((uint8_t) (found_queries + found_queries/255));
+
+            found_queries = found_queries * (query_len + 1);
+
+            *text_offset.i += found_queries;
+
+            *query_offset.i += found_queries;
+        }
+
+
+/*
+
         //if match is found
         if((bool)(*query_matches.i = reduce(value))) {
             //if pattern is present at current shift
@@ -167,13 +265,13 @@ int search_test (unsigned char* query_array,
                 while (finished_queries_offset_plus_query_len > 0) {
 
                 }
-
             } else {
                 //s += max(1, j - badchar[txt[s+j]]);
             }
         } else {
             //s += max(1, j - badchar[txt[s+j]]);
         }
+        */
     }
     //We return False if the text is searched and nothing is found.
     return result;
